629904b78958c2e3cf0952c05609c8e466d50eb7
DefNotArham <arhamkabir231@gmail.com>
2026-08-01 19:52:49 -0400

    add digits

--- 11-add-digits.js
+++ 11-add-digits.js
@@ -1,14 +1,18 @@
 var addDigits = function (num) {
-  const numString = String(num);
-  const splitString = numString.split("");
+  while (num >= 10) {
+    const numString = String(num);
+    const splitString = numString.split("");
 
-  let sum = 0;
+    let sum = 0;
 
-  for (let i = 0; i < splitString.length; i++) {
-    sum = sum + Number(splitString[i]);
+    for (let i = 0; i < splitString.length; i++) {
+      sum = sum + Number(splitString[i]);
+    }
+
+    num = sum;
   }
 
-  return sum;
+  return num;
 };
 
-console.log(addDigits(12));
+console.log(addDigits(11));
